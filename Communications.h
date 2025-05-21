52e2060bbfb9f6738e0308ad72a57421f1c5707c
AyaanG123 <160043446+AyaanG123@users.noreply.github.com>
2025-05-20 21:00:59 -0400

    Create Communications.h

---
+++ Communications.h
@@ -0,0 +1,9 @@
+#ifndef COMMUNICATIONS_H //include guards to prevent multiple inclusions
+#define COMMUNICATIONS_H
+#include <iostream>
+
+std::string greeting(std::string name);
+void addy(std::string &street); //declaring the function
+std::string closing(std::string product);
+
+#endif//end of include guards
